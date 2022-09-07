7615ff835e6875c1d8b33c8a179a350fc57e6ae0
Draugwath <kulchitskydmitry@gmail.com>
2022-09-07 21:09:41 +0400

    'release'

--- viceversa/views.py
+++ viceversa/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 def home(request):
     return render(request, 'home.html')
+def reverse(request):
+    source_text = request.GET['usertext']
+    reverse_text = str(source_text)[::-1]
+    return render(request, 'reverse.html', {'reverse_text':reverse_text})
