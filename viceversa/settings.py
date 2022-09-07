0ed8b03300723638f5aa14009605782af82733ee
draugwath <110353611+draugwath@users.noreply.github.com>
2022-09-07 21:17:21 +0400

    Update settings.py

--- viceversa/settings.py
+++ viceversa/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e-)va2*ff^ph+hx&)t!*%9lkg3@kmua#0-9e&b%q=vcx1os084'
+SECRET_KEY = 'XXX'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
